d4b6c16ae7c30d362f6325c048a037b679f33418
Flynn Tesoriero <flynn@flynntes.com>
2021-09-30 16:32:14 +1000

    Added route for getting prices for station

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -5,12 +5,23 @@ const Station = require("../models/stations")
 
 const apiRouter = express.Router()
 
+// Get list of all prices
 apiRouter.get('/api/prices', (req, res) => {
     Price.find({}).then(result => {
         res.json(result)
     })
 })
 
+// Get prices for station
+apiRouter.get('/api/prices/:stationcode', (req, res) => {
+    Price.find({
+        stationcode: req.params.stationcode
+    }).then(result => {
+        res.json(result)
+    })
+})
+
+// Get list of all stations
 apiRouter.get('/api/stations', (req, res) => {
     Station.find({}).then(result => {
         res.json(result)
