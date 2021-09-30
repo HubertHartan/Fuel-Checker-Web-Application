c4bad5ca6ea46be6359c91fe48bb85ce4ff17996
Flynn Tesoriero <flynn@flynntes.com>
2021-09-30 16:52:01 +1000

    Added station table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import {
+  Row,
+  Container,
+  Col,
+} from 'react-bootstrap'
+
 import './App.css';
 
 import Navigation from './components/Navigation'
+import StationTable from './components/StationTable'
 
 function App() {
+  const baseUrl = 'http://localhost:3001/api'
+  const [stations, setStations] = useState([])
+
+  useEffect(() => {
+    axios.get(`${baseUrl}/stations`)
+      .then(response => {
+        setStations([...response.data])
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }, [])
+
   return (
     <>
       <Navigation />
+      <Container>
+        <Row>
+          <Col>
+            <StationTable stations={stations} />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
---
+++ src/components/StationTable.js
@@ -0,0 +1,34 @@
+import {
+    Table
+} from 'react-bootstrap'
+
+const Navigation = ({stations}) => {
+    const listOfStations = stations.map((station) => {
+        return (
+            <tr key={station.code}>
+                <td>{station.brand}</td>
+                <td>{station.name}</td>
+                <td>{station.state}</td>
+            </tr>
+        )
+    })
+
+    return (
+        <>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Brand</th>
+                        <th>Name</th>
+                        <th>State</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listOfStations}
+                </tbody>
+            </Table>
+        </>
+    )
+}
+
+export default Navigation
