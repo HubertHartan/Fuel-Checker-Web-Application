b02aea4efaf7eda78a2f5b2ef39987f02e66bf38
Flynn Tesoriero <flynn@flynntes.com>
2021-10-10 13:55:44 +1100

    Added styling to header

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,39 @@
 import {
     Navbar,
     Container,
+    NavDropdown,
+    Nav,
+    Button
 } from 'react-bootstrap'
+import { DropletHalf } from 'react-bootstrap-icons'
 
 const Navigation = ({ }) => {
     return (
         <>
             <Navbar bg="light">
                 <Container>
-                    <Navbar.Brand href="#home">Fuel Checker</Navbar.Brand>
+                    <Navbar.Brand href="#home">
+                        <DropletHalf className="d-inline-block align-middle me-2 text-primary" />
+                        <span>Fuel Checker</span>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Sign In</Nav.Link>
+                            <Button variant="primary">Sign Up</Button>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </>
