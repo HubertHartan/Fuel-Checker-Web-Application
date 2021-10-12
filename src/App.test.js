ee191dd6e4d2b8d3c0fb6a2bef07806faf3e5243
TrideepLD <trideeplaldas@hotmail.com>
2021-10-13 05:38:09 +1100

    Added Jest Test. Will need testing on other systems

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
+const mongoose = require('mongoose') 
+const supertest = require('supertest')
+const fs = require('fs')
+const request = require("supertest");
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe("GET / ", () => {
+  test("Should get 200", async () => {
+    const response = await request(App).get("/");
+    expect(response.statusCode).toBe(200);
+    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 10000);
+  });
 });
