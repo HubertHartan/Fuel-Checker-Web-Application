f739f101560aaeb443cf89b650f4206be2e39973
TrideepLD <trideeplaldas@hotmail.com>
2021-10-14 08:38:21 +1100

    Added Backend Testing stuff

---
+++ server/tests/app.test.js
@@ -0,0 +1,37 @@
+/**
+ * @jest-environment node
+ */
+
+ const mongoose = require('mongoose') 
+ const supertest = require('supertest')
+ const fs = require('fs')
+ const app = require('../app')
+ const users = require("../models/users")
+ 
+ const api = supertest(app)
+
+ const sampleData =  async (fileName) => {
+    const rawData = fs.readFileSync(fileName)
+    const data = JSON.parse(rawData)
+
+    // use a for loop rather than map because we want await
+    for(let i=0; i<data.users.length; i++) {
+        const record = data.users[i]
+        const l = new users(record)
+        await l.save() 
+    }
+}
+
+describe('api', () => {
+
+    test('Expecting Status 200', async () => {
+        await api.get('/')
+            .expect(200)
+    })
+
+    test('Simple test expecting page to be HTML', async () => {
+        await api.get('/')
+                .expect('Content-Type', "text/html; charset=utf-8")
+    })
+
+})
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment node
+ */
+
 import { render, screen } from '@testing-library/react';
 const mongoose = require('mongoose') 
 const supertest = require('supertest')
@@ -5,16 +9,21 @@ const fs = require('fs')
 const request = require("supertest");
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+const api = supertest(App)
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 
 describe("GET / ", () => {
-  test('get request returns JSON', async () => {
-    await api.get('/')
+
+  test('get request returns', async () => {
+    await api.get('/api')
             .expect(200)
             .expect('Content-Type', /application\/json/)
-  })
+})
+
 });
