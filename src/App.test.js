0ad2f324e07ff9ef4b74f3131a04bc28d6e30aa1
TrideepLD <trideeplaldas@hotmail.com>
2021-10-21 09:20:19 +1100

    Added some frontend Tests

--- src/App.test.js
+++ src/App.test.js
@@ -7,6 +7,9 @@ import renderer from "react-test-renderer";
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import Dashboard from "./components/Dashboard"
+import Map from "./components/Map";
+import MapContainer from "./components/MapContainer";
+import MetricCard from "./components/MetricCard";
 
 
 test('renders learn react link', () => {
@@ -15,9 +18,33 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test('snapshot test', () => {
+test('Dashboard snapshot test', () => {
   const component = render(
     <Dashboard fuelType={fuelType} metrics={metrics} stations={stations}/>
+  )
+  
+  expect(component).toMatchSnapshot()
+})
+
+test('Map snapshot test', () => {
+  const component = render(
+    <Map title={title} figure={figure}/>
+  )
+  
+  expect(component).toMatchSnapshot()
+})
+
+test('MapContainer snapshot test', () => {
+  const component = render(
+    <MapContainer initialLocation={initialLocation}/>
+  )
+  
+  expect(component).toMatchSnapshot()
+})
+
+test('MetricCard snapshot test', () => {
+  const component = render(
+    <MetricCard title={title} figure={figure}/>
   )
   
   expect(component).toMatchSnapshot()
