b078a9be6d092f428625bd23f1c79055cf0e7d65
Flynn Tesoriero <flynn@flynntes.com>
2021-10-28 10:20:31 +1100

    Added header and layout to station table screen

--- src/components/StationTable.js
+++ src/components/StationTable.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import {
-  Table
+  Table,
+  Container,
+  Row,
+  Col
 } from 'react-bootstrap'
 import userService from "../services/user"
 import { useAuth0 } from "@auth0/auth0-react"
@@ -30,19 +33,26 @@ const Navigation = ({ stations }) => {
 
   return (
     <>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Brand</th>
-            <th>Name</th>
-            <th>State</th>
-            <th>Add</th>
-          </tr>
-        </thead>
-        <tbody>
-          {listOfStations}
-        </tbody>
-      </Table>
+      <Container className="py-4">
+        <Row>
+          <Col>
+            <h2 className="fw-bold mb-4">Stations</h2>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Brand</th>
+                  <th>Name</th>
+                  <th>State</th>
+                  <th>Add</th>
+                </tr>
+              </thead>
+              <tbody>
+                {listOfStations}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
