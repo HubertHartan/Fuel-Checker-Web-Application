b2dbf9fa870316f461142bed91abe900633dd2dd
TrideepLD <trideeplaldas@hotmail.com>
2021-11-06 06:57:19 +1100

    Testing FuelTypes

--- server/tests/app.test.js
+++ server/tests/app.test.js
@@ -21,12 +21,27 @@ describe('api', () => {
   test('The Station API is reachable', async () => {
     await api.get('/api/stations')
     .expect(200);
-})
+  })
+
+
+
+  test('The Map API works', async () => {
+    await api.get('/api/map/geojson')
+    .expect(200);
+  })
 
   test('Simple test expecting page to be JSON', async () => {
     await api.get('/')
       .expect('Content-Type', 'application/json; charset=utf-8')
   })
+
+  test('How does this work?', async () => {
+    await api.get('/api/prices/fuel/:fueltype')
+    .expect(200);
+  })
+  
+
+
 
   afterAll(async () => {
     await new Promise(resolve => setTimeout(() => resolve(), 500)); // avoid jest open handle error
